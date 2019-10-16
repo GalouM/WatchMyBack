b0b83cda5c27a861f4e965a47809faf03be98d67
GalouM <galou.minisini@gmail.com>
2019-10-16 14:45:30 -0700

    interface main

--- app/app/src/main/java/com/galou/watchmyback/MainActivity.kt
+++ app/app/src/main/java/com/galou/watchmyback/MainActivity.kt
@@ -2,11 +2,25 @@ package com.galou.watchmyback
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureToolbar()
+    }
+
+    private fun configureToolbar(){
+        val toolbar = findViewById<Toolbar>(R.id.main_activity_toolbar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.main_activity_drawer_layout)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+            R.string.open_nav_drawer, R.string.close_nav_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
