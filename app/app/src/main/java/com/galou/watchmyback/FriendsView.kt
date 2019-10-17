fb9680aaff136db87f203cb63f8ffd9737ac2a3c
GalouM <galou.minisini@gmail.com>
2019-10-17 14:13:52 -0700

    xml all main views

---
+++ app/app/src/main/java/com/galou/watchmyback/FriendsView.kt
@@ -0,0 +1,24 @@
+package com.galou.watchmyback
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FriendsView : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends_view, container, false)
+    }
+
+
+}
