6636151aa8f53635a272bd36e7ed6fb4f0837ecd
Galou Minisini <galou.minisini@gmail.com>
2019-10-21 10:00:41 -0700

    docstring other util

--- app/app/src/main/java/com/galou/watchmyback/utils/otherUtils.kt
+++ app/app/src/main/java/com/galou/watchmyback/utils/otherUtils.kt
@@ -6,10 +6,17 @@ import java.util.*
  * Created by galou on 2019-10-20
  */
 
+/**
+ * Generate a unique and random string using the class UUID
+ */
 var idGenerated: String = ""
     get() {
         field = UUID.randomUUID().toString()
         return field
     }
 
+
+/**
+ * Generate todays date as Date using the class Calendar
+ */
 var todaysDate: Date = Calendar.getInstance(Locale.CANADA).time
