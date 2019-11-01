96a36ad4a95a5ed7d5d71a8ac3d2c42eaf59cc84
GalouM <galou.minisini@gmail.com>
2019-11-01 10:54:31 -0700

    test settings activity

--- app/app/src/androidTest/java/com/galou/watchmyback/SettingActivityTest.kt
+++ app/app/src/androidTest/java/com/galou/watchmyback/SettingActivityTest.kt
@@ -5,24 +5,27 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.galou.watchmyback.data.entity.TimeDisplay
+import com.galou.watchmyback.data.entity.UnitSystem
 import com.galou.watchmyback.data.entity.UserPreferences
 import com.galou.watchmyback.data.repository.UserRepository
 import com.galou.watchmyback.instrumentedTestHelpers.*
 import com.galou.watchmyback.settings.SettingsActivity
 import com.galou.watchmyback.settings.SettingsViewModel
 import com.galou.watchmyback.utils.Result
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.test.KoinTest
 import org.koin.test.mock.declareMock
 import org.mockito.BDDMockito
@@ -35,6 +38,9 @@ import org.mockito.BDDMockito.given
 class SettingActivityTest : KoinTest {
 
     private val user = generateTestUser(TEST_UID)
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
 
     @get:Rule
@@ -106,5 +112,91 @@ class SettingActivityTest : KoinTest {
         onView(withId(R.id.settings_view_notification_back_switch))
             .check(matches(hasCorrectValue(TEST_NOTIFICATION_BACK)))
 
+        when(preferencesTest.unitSystem){
+            UnitSystem.METRIC -> {
+                onView(withId(R.id.settings_view_unit_system_unit_metric))
+                    .check(matches(isChecked()))
+            }
+            UnitSystem.IMPERIAL -> {
+                onView(withId(R.id.settings_view_unit_system_unit_imperial))
+                    .check(matches(isChecked()))
+            }
+        }
+
+        when(preferencesTest.timeDisplay){
+            TimeDisplay.H_24 -> {
+                onView(withId(R.id.settings_view_unit_system_time_24))
+                    .check(matches(isChecked()))
+            }
+            TimeDisplay.H_12 -> {
+                onView(withId(R.id.settings_view_unit_system_time_12))
+                    .check(matches(isChecked()))
+            }
+        }
+
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun onClickNotifications_update_preferences(){
+        val preferences = activityTestResult.activity.viewModel<SettingsViewModel>().value.preferencesLD
+
+        if(preferences.value?.notificationEmergency == false){
+            onView(withId(R.id.settings_view_notification_emergency_switch))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.notificationEmergency, true)
+
+        if(preferences.value?.notificationBackSafe == true){
+            onView(withId(R.id.settings_view_notification_back_switch))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.notificationBackSafe, false)
+
+        if(preferences.value?.notificationLate == true){
+            onView(withId(R.id.settings_view_notification_late_switch))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.notificationLate, false)
+
+        if(preferences.value?.notificationLocationUpdate == true){
+            onView(withId(R.id.settings_view_notification_update_switch))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.notificationLocationUpdate, false)
+
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun clickMetricData_updatePreferences(){
+        val preferences = activityTestResult.activity.viewModel<SettingsViewModel>().value.preferencesLD
+
+        if(preferences.value?.unitSystem == UnitSystem.METRIC){
+            onView(withId(R.id.settings_view_unit_system_unit_imperial))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.unitSystem, UnitSystem.IMPERIAL)
+
+        if(preferences.value?.timeDisplay == TimeDisplay.H_24){
+            onView(withId(R.id.settings_view_unit_system_time_12))
+                .perform(click())
+        }
+        assertEquals(preferences.value?.timeDisplay, TimeDisplay.H_12)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun changeEmergencyNumber_updatePreferences() {
+        val preferences = activityTestResult.activity.viewModel<SettingsViewModel>().value.preferencesLD
+
+        val newNumber = "112"
+        onView(withId(R.id.settings_view_emergency_number))
+            .perform(replaceText(newNumber))
+        assertEquals(preferences.value?.emergencyNumber, newNumber)
+    }
+
+
+
 }
