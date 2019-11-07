67ba8b3062e24861b380302005367f0fe34f164f
GalouM <galou.minisini@gmail.com>
2019-11-07 12:58:19 -0800

    downgraded google service

---
+++ app/app/src/main/java/com/galou/watchmyback/addFriend/AddFriendViewModel.kt
@@ -0,0 +1,11 @@
+package com.galou.watchmyback.addFriend
+
+import com.galou.watchmyback.base.BaseViewModel
+
+/**
+ * @author galou
+ * 2019-11-07
+ */
+class AddFriendViewModel : BaseViewModel() {
+
+}
