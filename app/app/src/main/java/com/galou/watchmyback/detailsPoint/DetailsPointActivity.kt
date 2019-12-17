653f33bb0c688924912f80bc55a8ffcd94a82329
GalouM <galou.minisini@gmail.com>
2019-12-16 20:14:40 -0800

    details point activity done
    Signed-off-by: GalouM <galou.minisini@gmail.com>

--- app/app/src/main/java/com/galou/watchmyback/detailsPoint/DetailsPointActivity.kt
+++ app/app/src/main/java/com/galou/watchmyback/detailsPoint/DetailsPointActivity.kt
@@ -2,6 +2,7 @@ package com.galou.watchmyback.detailsPoint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.galou.watchmyback.R
 
 class DetailsPointActivity : AppCompatActivity() {
@@ -9,5 +10,16 @@ class DetailsPointActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_point)
+        configureToolbar()
+    }
+
+    //-------------------------
+    //  CONFIGURE UI
+    //-------------------------
+    private fun configureToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
